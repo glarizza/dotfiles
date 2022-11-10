a6fb319268998ee55ea822f3024a1f4741b63814
Gary Larizza <glarizza@me.com>
2022-11-10 15:41:16 -0800

    Updates for Ventura
    Changes I needed to make to setup my environment with Ventura. Finally
    got rid of oh-my-zsh in favor of all p10k.

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,11 +18,10 @@ DOTFILES="${SRCDIR}/dotfiles"
 [[ ! -h "${HOMEDIR}/.p10k.zsh" ]] && ln -s "${DOTFILES}/p10k.zsh" "${HOMEDIR}/.p10k.zsh"
 [[ ! -h "${HOMEDIR}/.tmux.conf" ]] && ln -s "${DOTFILES}/tmux.conf" "${HOMEDIR}/.tmux.conf"
 [[ ! -d "${HOMEDIR}/.vim/colors" ]] && mkdir -p "${HOMEDIR}/.vim/colors"
-[[ ! -f "${HOMEDIR}/.vim/colors/solarized.vim" ]] && git clone git://github.com/altercation/vim-colors-solarized.git "${HOMEDIR}/src/vim-colors-solarized" && cp "${HOMEDIR}/src/vim-colors-solarized/colors/solarized.vim" "${HOMEDIR}/.vim/colors/solarized.vim"
+[[ ! -f "${HOMEDIR}/.vim/colors/solarized.vim" ]] && git clone https://github.com/altercation/vim-colors-solarized.git "${HOMEDIR}/src/vim-colors-solarized" && cp "${HOMEDIR}/src/vim-colors-solarized/colors/solarized.vim" "${HOMEDIR}/.vim/colors/solarized.vim"
 
 # ZSH Configuration
-[[ ! -d "${HOMEDIR}/.oh-my-zsh" ]] && git clone https://github.com/glarizza/oh-my-zsh.git "${HOMEDIR}/.oh-my-zsh"
-[[ ! -d "${HOMEDIR}/.oh-my-zsh/themes/powerlevel10k" ]] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${HOMEDIR}/.oh-my-zsh/themes/powerlevel10k"
+[[ ! -d "${HOMEDIR}/powerlevel10k" ]] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${HOMEDIR}/powerlevel10k"
 if [[ "/bin/zsh" != "${SHELL}" ]]; then
     chsh -s /bin/zsh
     echo "NOTE: You will need to restart for the ZSH shell change to take effect"
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,22 +1,3 @@
-#############
-# oh-my-zsh #
-#############
-
-# Path to your oh-my-zsh configuration.
-ZSH=$HOME/.oh-my-zsh
-
-# time that oh-my-zsh is loaded.
-#ZSH_THEME="garylarizza"
-ZSH_THEME="powerlevel10k/powerlevel10k"
-
-# Comment this out to disable weekly auto-update checks
- DISABLE_AUTO_UPDATE="true"
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git)
-
-source $ZSH/oh-my-zsh.sh
-
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+source ~/powerlevel10k/powerlevel10k.zsh-theme
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
